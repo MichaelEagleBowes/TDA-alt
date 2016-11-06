fb848226e65ff5ecaa3d1a14cc413383f2d3968f
Florian Beetz <florian1.beetz@stud.uni-bamberg.de>
2016-11-06 23:21:06 +0100

    Added documentation for Parser

--- TDA/src/de/uni_bamberg/swl/tda/parser/Parser.java
+++ TDA/src/de/uni_bamberg/swl/tda/parser/Parser.java
@@ -2,6 +2,9 @@ package de.uni_bamberg.swl.tda.parser;
 
 import de.uni_bamberg.swl.tda.logic.TestRun;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import javax.xml.stream.XMLEventReader;
@@ -17,6 +20,26 @@ public class Parser {
 
     private static final XMLInputFactory FACTORY = XMLInputFactory.newFactory();
 
+    /**
+     * Parses XML from a {@link File} to a {@link TestRun}.
+     *
+     * @param file the file to parse.
+     * @return the parsed {@link TestRun}.
+     * @throws FileNotFoundException Signals that the given file could not be found.
+     * @throws ParserException Signals that an error occurred while parsing.
+     * @see #parse(InputStream)
+     */
+    public static TestRun parse(File file) throws FileNotFoundException, ParserException {
+        return parse(new FileInputStream(file));
+    }
+
+    /**
+     * Parses XML form an {@link InputStream} to a {@link TestRun}.
+     *
+     * @param stream the input stream.
+     * @return the parsed {@link TestRun}.
+     * @throws ParserException Signals that an error occurred while parsing.
+     */
     public static TestRun parse(InputStream stream) throws ParserException {
         try {
             XMLEventReader reader = FACTORY.createXMLEventReader(stream);
