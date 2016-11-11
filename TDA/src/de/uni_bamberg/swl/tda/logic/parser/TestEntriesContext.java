a6b2a13ccbe80d84d8ffb4b46ab61c01e322b182
MichaelEagleBowes <romab1@web.de>
2016-11-11 20:52:48 +0100

    Update TestEntriesContext.java

--- TDA/src/de/uni_bamberg/swl/tda/logic/parser/TestEntriesContext.java
+++ TDA/src/de/uni_bamberg/swl/tda/logic/parser/TestEntriesContext.java
@@ -1,20 +1,52 @@
 package de.uni_bamberg.swl.tda.logic.parser;
 
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+
 import de.uni_bamberg.swl.tda.logic.TdaDataModelException;
 import de.uni_bamberg.swl.tda.logic.TestRun;
-import de.uni_bamberg.swl.tda.logic.TestRunBuilder;
 
 /**
- * @author
+ * Parses the {@code <TestEntries>} element in the XML.
+ * 
+ * @author Michael Bowes
  */
 public class TestEntriesContext extends ParsingContextBase {
 
-    // TODO: implement me
+	private final ParsingContext parent;
 
-    public TestEntriesContext(ParsingContext parent, TestRunBuilder builder) {}
+    public TestEntriesContext(ParsingContext parent) {
+        this.parent = parent;
+    }
+	
+	/**
+     * {@inheritDoc}
+     */
+    @Override
+    protected ParsingContext handleStartElement(StartElement startElement) throws ParserException {
+        return super.handleStartElement(startElement); // TestEntries are not parsed
+    }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected ParsingContext handleEndElement(EndElement endElement) throws ParserException {
+        if (XMLName.Element.TEST_ENTRIES.equals(endElement.getName().getLocalPart())) {
+            return parent; // switch back to parent context
+        }
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public TestRun getResult() throws IllegalStateException, TdaDataModelException {
-        return null;
+        throw new IllegalStateException(
+                String.format("The result of the parsing operation could not be determined. " +
+                                "The currently parsed element is '%s'.",
+                        XMLName.Element.TEST_ENTRIES)
+        );
     }
 }
